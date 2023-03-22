5bac87c974e4ef5b2b933f599ae004310c76ee60
Luke Collins <lgcollins@deakin.edu.au>
2023-03-22 12:55:39 +1100

    Fixed error variable on error handler

--- express-arithmetic-api/src/app/modules/routed/api/index.js
+++ express-arithmetic-api/src/app/modules/routed/api/index.js
@@ -11,7 +11,7 @@ router.use('/division', require('./division'));
 router.use('/modulation', require('./modulation'));
 
 // Handle all unmatched routes with a 404 error
-router.use(function (req, res, next) {
+router.use(function (error, req, res, next) {
     logger.error(`[${fn}]: Unmatched Route. Please check the error log for more information.`, error.details);
     res.status(404).json({
         error: 'Route not found. Available routes: api/addition/:num1/:num2, api/subtraction/:num1/:num2, api/multiplication/:num1/:num2, api/division/:num1/:num2, api/modulation/:num1/:num2'
